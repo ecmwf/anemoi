4577e0665dc53ffe4f1eb8170c037633e5bf49c9
Lars Falk-Petersen <larsfp@met.no>
2026-02-27 15:17:03 +0100

    Hack in retrieve.sh to fix metadata bug

--- tests/system-level/anemoi_test/configs/inference/stretched/retrieve.sh
+++ tests/system-level/anemoi_test/configs/inference/stretched/retrieve.sh
@@ -4,6 +4,9 @@ MARS_FILE=mars_request.txt
 # Generate mars request
 $RETRIEVE_CMD $CONFIG_PATH --output $MARS_FILE
 
+# Fix bug in metadata
+sed -i 's/origin=4/origin=se-al-ec/g' $MARS_FILE
+
 # Get data from mars
 $MARS_CMD $MARS_FILE
 
