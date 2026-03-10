3cd078126b9811e9ecc9cadb4e74b50f3f481686
Lars Falk-Petersen <larsfp@met.no>
2026-03-10 09:16:07 +0100

    Split input in lam and global

--- tests/system-level/anemoi_test/configs/inference/stretched/retrieve.sh
+++ tests/system-level/anemoi_test/configs/inference/stretched/retrieve.sh
@@ -1,3 +1,5 @@
+echo "Retrieving global data"
+
 # Define environment variables
 MARS_FILE=mars_request.txt
 
@@ -12,4 +14,24 @@ $MARS_CMD $MARS_FILE
 
 # Copy files to output directory
 mkdir -p $OUTPUT_PATH
-mv *.grib $OUTPUT_PATH/
+mv *.grib $OUTPUT_PATH/global.grib
+
+################################################################################
+
+echo "Retrieving lam data"
+
+# Define environment variables
+MARS_FILE=mars_request.txt
+
+# Generate mars request
+$RETRIEVE_CMD $CONFIG_PATH --output $MARS_FILE
+
+# Overwrite metadata error. TODO: fix metadata instead
+sed -i 's/origin=4/origin=se-al-ec/g' $MARS_FILE
+
+# Get data from mars
+$MARS_CMD $MARS_FILE
+
+# Copy files to output directory
+mkdir -p $OUTPUT_PATH
+mv *.grib $OUTPUT_PATH/lam.grib
