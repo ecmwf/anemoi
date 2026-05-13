840621a3d3fc3afa1fbb256fdfa2ffb78a6d0559
Lars Falk-Petersen <larsfp@met.no>
2026-05-13 12:56:37 +0200

    Check for grib _OR_ netcdf output.

--- tests/system-level/anemoi_test/configs/inference/basic_check.sh
+++ tests/system-level/anemoi_test/configs/inference/basic_check.sh
@@ -4,7 +4,7 @@ if [[ ! -d "$OUTPUT_PATH" ]]; then
 fi
 cd "$OUTPUT_PATH"
 
-if [[ ! -f output.grib ]]; then
+if [[ ! -f output.grib  && ! -f output.nc ]]; then
   echo "❌ Output file not found: output.grib"
   exit 1
 fi
